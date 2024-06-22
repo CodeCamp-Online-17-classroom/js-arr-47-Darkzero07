b37cd0a33cbe11097203d93443c8ad28ddad0984
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-22 13:12:18 +0700

    js lab 133

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+// จงเขียนฟังก์ชัน copySorted(arr) เพื่อ return array ที่เรียงข้อมูลใน arr จากน้อยไปหามาก โดยที่ arr ต้องมีค่าเหมือนเดิม
+
+let arr = ["React", "Vue", "Angular"];
+
+function copySorted(arr) {
+  return [...arr].sort();
+}
+let sorted = copySorted(arr);
+
+console.log(sorted); // Angular, React, Vue
+console.log(arr); // React, Vue, Angular (no changes)
